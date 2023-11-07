a5e34786717326b69c07721393356161d060a62a
janisepulveda <janis.sepulveda@mail.udp.cl>
2023-11-07 17:57:03 -0300

    agregar cambios

--- welcomingLamp_28-10/welcomingLamp_28-10.ino
+++ welcomingLamp_28-10/welcomingLamp_28-10.ino
@@ -1,5 +1,7 @@
-// libreria del modulo reproductor mp3
-#include <DFMiniMp3.h>
+#include "Arduino.h"
+
+// libreria para reproducir el audio
+#include "DFRobotDFPlayerMini.h"
 
 // libreria que nos va a servir para acortar la distancia de medicion del sensor
 #include <NewPing.h>
@@ -8,52 +10,34 @@
 #define TRIGGER_PIN 8
 #define ECHO_PIN 9 
 #define MAX_DISTANCE 60
-#define SPEAKER_PIN 6
-#define MP3_PIN
-// const int pinEcho = 9;
-// const int pinTrigger = 8;
-// const int pinRelay = 3;
-// const int pinSpeaker = 4;
-// const int pinMP3 = 5;
+// #define SPEAKER_PIN 
+#define MP3_PIN 10
+#define MP3_PIN2 11
+
+// definir reproductor mp3 y sus respectivos pines
+#if (defined(ARDUINO_AVR_UNO) || defined(ESP8266)) 
+
+// comunicacion serial
+#include <SoftwareSerial.h>
+SoftwareSerial softSerial(/*rx =*/10, /*tx =*/11);
+#define FPSerial softSerial
+#else
+#define FPSerial Serial1
+#endif
 
 
 // acorta la distancia del sensor, (pinTrigger, pinEcho, distancia maxima en cm)
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, 60);
 
-// inicia el reproductor mp3
-// DFPlayerMini mp3(pinMP3);
-
-// funcion para reproducir un archivo de audio
-int playFile() {
-  // numero de archivos de audio en la SD
-  // int numFiles = SD.countFiles();
-
-  // generar un numero aleatorio entre 0 y el numero total de archivos
-  // int randomFile = random(numFiles);
-
-  // devolver el numero aleatorio
-  // return randomFile;
-}
-
 void setup() {
   Serial.begin(9600);
-
-  // iniciar el sensor hc
-  // sonar.begin();
-
-  // iniciar la comunicacion spi
-  // SPI.begin();
-
-  // iniciar la tarjeta SD
-  // SD.begin();
-
   configurarPines();
-
-  // reproducir el archivo de audio de inicio
-  playFile();
+  configurarMp3();
 }
 
 void loop() {
+
+  configurarPines();
   Serial.print("Distancia: ");
   Serial.print(sonar.ping_cm());
   Serial.println("cm");
@@ -64,12 +48,9 @@ void loop() {
   // si la cantidad de personas es mayor a 1, enciende la ampolleta y reproduce el audio aleatorio
   if (sonar.ping_cm() >= 1 && sonar.ping_cm() <= 60) {
     digitalWrite(LED_BUILTIN, HIGH);
-    int randomFile = playFile();
-    // mp3.playRandom(randomFile);
 
   // si la medicion es 0, apaga la ampolleta y deten la reproduccion del audio aleatorio
   } else {
     digitalWrite(LED_BUILTIN, LOW);
-    // mp3.stop();
   }
 }
